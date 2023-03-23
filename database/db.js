c4a8c80c8804ce476e36eb93f8c585fe39a34deb
vikaspawan <76751462+vikaspawan@users.noreply.github.com>
2023-03-24 01:33:45 +0530

    Database commit

---
+++ database/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config()
+
+//URL for database connection
+const dbURL = process.env.dbURL;
+
+//function to connect database
+const connectToMongo = () => {
+    mongoose.connect(dbURL).then(() => {
+        console.log("Connected to database")
+    }).catch((err) => console.log("No Connection From Database"));
+}
+module.exports = connectToMongo;
